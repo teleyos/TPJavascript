2c2401d61157e1f2ee787de02ecb95ba931d6edd
teleyos <tristanduret@hotmail.fr>
2021-03-16 16:23:35 +0100

    t

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function loadDoc(){
 	xmlhttp=new XMLHttpRequest()
 
 	xmlhttp.onreadystatechange = () => {
-    console.log(this.readyState+"  "+this.status+"   "+this.responseText);
+    document.write(xmlhttp.responseText);
   }
 	
 	xmlhttp.open("GET","./web.php",true);
