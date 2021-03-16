c4ece253197ff25724855949ff1f4393473ac6c7
teleyos <tristanduret@hotmail.fr>
2021-03-16 16:13:08 +0100

    t

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
         <script type="text/javascript" src="script.js"></script>
     </head>
     <body>
+        <div id="response"></div>
     </body>
 </html>
--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+xmlhttp=new XMLHttpRequest()
+
+xmlhttp.onreadystatechange = () => {
+  document.getElementById("response").innerHTML=xmlhttp.responseText;
+}
+
+xmlhttp.open("GET","web.php",false);
+xmlhttp.send();
+
 function separateur(texte, separateur){
     return texte.split(separateur);
 }
