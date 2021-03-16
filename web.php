17a6bc8c92e5043a32a6e701c766a683f23e0e07
teleyos <tristanduret@hotmail.fr>
2021-03-16 16:57:12 +0100

    t

--- web.php
+++ web.php
@@ -4,10 +4,11 @@ include_once("connexion.php");
 
 $req="select * from CLient";
 $result = mysqli_query($conn,$req);
-#
-#while($ligne->mysqli_fetch_array($result)){
-#    echo $ligne['prenom'];
-#}
-#
-#mysqli_close($conn);
+
+while($ligne==mysqli_fetch_array($result)){
+    echo $ligne['prenom'];
+}
+
+
+mysqli_close($conn);
 ?>
