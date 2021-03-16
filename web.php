1ecddaa18204494f9f19ebe038c393ec955b9abf
teleyos <tristanduret@hotmail.fr>
2021-03-16 16:43:57 +0100

    t

--- web.php
+++ web.php
@@ -1,21 +1,17 @@
 <?php 
 
 include_once("connexion.php");
-
-function requete($req){
-    $result = $conn->prepare($req);
-    $result->fetchAll();
-    
-    if(count($result) > 0){
-        while($result = $result->fetch()){
-            echo $result['prenom']."<br/>";
-        }
-    }else{
-        error("empty set or error");
+$req="select * from CLient";
+$result = $conn->prepare($req);
+$result->fetchAll();
+  
+if(count($result) > 0){
+    while($result = $result->fetch()){
+        echo $result['prenom']."<br/>";
     }
+}else{
+    error("empty set or error");
 }
-
-requete("select * from Client");
 
 $conn->close();
 ?>
